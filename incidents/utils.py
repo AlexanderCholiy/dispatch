8239b2d598aabe5069a692602bcb1da78b4ecf86
Alexander Choliy <a.choliy@newtowers.ru>
2025-10-01 11:35:46 +0300

    Умееьшили период заявок которые были распределены на диспетчеров

--- incidents/utils.py
+++ incidents/utils.py
@@ -190,7 +190,7 @@ class IncidentManager(IncidentValidator):
     def choice_dispatch_for_incident(
         yt_manager: Optional[YandexTrackerManager],
         max_incident_num_per_user: Optional[int] = None,
-        hour_back: int = 12,
+        hour_back: int = 3,
     ) -> Optional[User]:
         """
         Возвращает самого свободного диспетчера.
