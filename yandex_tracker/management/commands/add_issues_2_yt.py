120592c728eb4bb628261027d42c7a56ac4c6066
Alexander Choliy <alexander.choliy@gmail.com>
2025-10-08 13:56:17 +0300

    Настроили тайминги для обновления настройки полей в трекере

--- yandex_tracker/management/commands/add_issues_2_yt.py
+++ yandex_tracker/management/commands/add_issues_2_yt.py
@@ -25,7 +25,7 @@ yt_managment_logger = LoggerFactory(
 class Command(BaseCommand):
     help = 'Обновление данных в YandexTracker.'
 
-    min_seconds = 3
+    min_seconds = 1
 
     def handle(self, *args, **kwargs):
         tg_manager.send_startup_notification(__name__)
