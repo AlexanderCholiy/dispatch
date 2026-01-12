1469c0c7ce9447fb310ac68427a41724494cd8e3
Alexander Choliy <alexander.choliy@gmail.com>
2026-01-12 16:46:52 +0300

    Remove collected_static from git tracking

--- collected_static/js/incidents/update_incident_links.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const emailSort = localStorage.getItem('email_sort') || 'asc';
-  document.querySelectorAll('.incident-link').forEach(link => {
-      const url = new URL(link.href);
-      url.searchParams.set('email_sort', emailSort);
-      link.href = url.toString();
-  });
-});
