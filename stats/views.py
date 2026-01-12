c98c97509a4bc8928f799985f2bb2cdeedee559a
Alexander Choliy <alexander.choliy@gmail.com>
2026-01-12 17:14:55 +0300

    Улучшили внешний вид границ графиков

--- stats/views.py
+++ stats/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from users.utils import role_required
@@ -6,5 +7,5 @@ from users.utils import role_required
 
 @login_required
 @role_required()
-def incidents_stats(request):
+def incidents_stats(request: HttpRequest) -> HttpResponse:
     return render(request, 'stats/incidents_stats.html')
